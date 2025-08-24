d33260dc5b2f9d8cc432ee37036183d270948736
sreenivasulu960 <sreenivasulu02012003@gmail.com>
2025-08-24 20:11:25 +0530

    example 1

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+# create an instance of microsoft edge
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert 'Python' in driver.title
 
-driver.get('https://towardsdatascience.com/automating-submission-forms-with-python-94459353b03e/')
+# get the search box element
+search_box = driver.find_element(By.NAME, 'q')
+# clear the existing input like Search
+search_box.clear()
 
-print(driver.title)
+# enter the search query
+search_box.send_keys('pycon')
+search_box.send_keys(Keys.RETURN)
+assert 'No results found.' not in driver.page_source
+# close the browser
+input("press Enter to close the browser")
+driver.close()
